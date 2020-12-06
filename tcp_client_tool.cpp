cda5ee520b1fee5d31c4fd14aacab1ed0f4c2010
Xianzhuo Rong <xianzhuorong@XianzhuodeMacBook-Pro.local>
2020-12-07 01:10:40 +0800

    commit some

---
+++ tcp_client_tool.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include "c_public/tcp_client.h"
+
+// g++ tcp_client_tool.cpp c_public/tcp_client.cpp -o tcp_client_tool.out
+
+int main(int argc, char** argv){
+    char ip[64];
+    int port;
+    printf("ip: ");
+    scanf("%s", ip);
+    printf("port: ");
+    scanf("%d", &port);
+    TcpClient client;
+    client.Connect("127.0.0.1", 4000);
+    client.StartListen();
+    while (1)
+    {
+        char buffer[64];
+        scanf("%s", buffer);
+        client.Send(buffer, strlen(buffer));
+    }
+    client.Close();
+    return 0;
+}
